138450925c55df766be181fa930244444bbfa073
UsernameDiana <ms.strele@gmail.com>
2018-12-05 13:35:57 +0100

    Add utility package

---
+++ src/main/java/com/g4/blockchain/Utilities/FileWriter.java
@@ -0,0 +1,6 @@
+package com.g4.blockchain.Utilities;
+
+public class FileWriter {
+    // Creates file locally
+    // Stores a list of hashes to a file
+}
---
+++ src/main/java/com/g4/blockchain/Utilities/Hashing.java
@@ -0,0 +1,5 @@
+package com.g4.blockchain.Utilities;
+
+public class Hashing {
+    // Implement 2 different ways of hashing block info
+}
