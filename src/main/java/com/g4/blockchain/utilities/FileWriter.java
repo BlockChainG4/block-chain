f5c9ab3b7381a7c795bc4d097a03cb30f573a239
UsernameDiana <ms.strele@gmail.com>
2018-12-17 14:58:15 +0100

    Formating

--- src/main/java/com/g4/blockchain/utilities/FileWriter.java
+++ src/main/java/com/g4/blockchain/utilities/FileWriter.java
@@ -10,44 +10,41 @@ import java.util.List;
 @Service // filewriter in test know about th9
 public class FileWriter {
     // Creates file locally
-    //
-
-        public static void save(List<String> blockArray) throws Exception {
+    public static void save(List<String> blockArray) throws Exception {
         /*PrintWriter pw = new PrintWriter(new FileOutputStream("blockchain.txt"));
         for (String hash : blockArray)
             pw.println(hash);
         pw.close();*/
 
-      // Make sure you use \\ instead of \
-      String filePath = "FileMaker.txt";
-      Writer bufferedWriter = null;
+        // Make sure you use \\ instead of \
+        String filePath = "FileMaker.txt";
+        Writer bufferedWriter = null;
 
-      try {
+        try {
 
-          //Creating a file
-          Writer fileWriter = new java.io.FileWriter(filePath);
-          bufferedWriter = new BufferedWriter(fileWriter);
+            //Creating a file
+            Writer fileWriter = new java.io.FileWriter(filePath);
+            bufferedWriter = new BufferedWriter(fileWriter);
 
-          // Writing the content
-          for(String line : blockArray) {
-              bufferedWriter.write(line);
-              bufferedWriter.write(System.getProperty("line.separator"));
-          }
-      } catch (IOException e) {
-          System.out.println("Problem occurs when creating file " + filePath);
-          e.printStackTrace();
-      } finally {
+            // Writing the content
+            for (String line : blockArray) {
+                bufferedWriter.write(line);
+                bufferedWriter.write(System.getProperty("line.separator"));
+            }
+        } catch (IOException e) {
+            System.out.println("Problem occurs when creating file " + filePath);
+            e.printStackTrace();
+        } finally {
 
-          //Closing the file
-          if (bufferedWriter != null) {
-              try {
-                  bufferedWriter.close();
-              } catch (IOException e) {
-                  System.out.println("Problem occurs when closing file !");
-                  e.printStackTrace();
-              }
-          }
-      }
-
-  }
+            //Closing the file
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    System.out.println("Problem occurs when closing file !");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
